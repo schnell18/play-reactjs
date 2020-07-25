e1a19a00804c34b6377c2f258a9ea505deec0a43
Justin Zhang <schnell18@gmail.com>
2020-07-25 17:00:18 +0800

    Change to class component

--- todo/src/components/TodoItem.js
+++ todo/src/components/TodoItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+// functional component
+/*
 function TodoItem(props) {
   return (
       <div className="todo-item">
@@ -7,6 +9,19 @@ function TodoItem(props) {
         <p>{props.item.name}</p>
       </div>
   );
+}*/
+
+// class component
+class TodoItem extends React.Component {
+  render() {
+    const props = this.props
+    return (
+        <div className="todo-item">
+          <input type="checkbox" checked={props.item.completed}></input>
+          <p>{props.item.name}</p>
+        </div>
+    );
+  }
 }
 
 export default TodoItem;
