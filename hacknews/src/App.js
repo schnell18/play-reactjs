156346b26c4c49ff3642fbdf50195084e09c56ab
Justin Zhang <schnell18@gmail.com>
2021-04-05 23:02:22 +0800

    Simple react intro project

---
+++ hacknews/src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 40,
+    objectId: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectId: 1,
+  },
+];
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {
+          list.map(item =>
+            <div key={item.objectId}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+// function App() {
+//   const hello = 'Welcome to world of ReactJS';
+//   return (
+//     <div className="App">
+//       <h2>{hello}</h2>
+//     </div>
+//   );
+// }
+
+export default App;
